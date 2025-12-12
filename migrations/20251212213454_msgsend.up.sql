640d0905e7d529e8349d74044b8abc8fd876e9b4
BizKey <toonychannel@gmail.com>
2025-12-13 00:43:58 +0300

    .

--- migrations/20251212213454_msgsend.up.sql
+++ migrations/20251212213454_msgsend.up.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS msgsend (
     args_price VARCHAR,
     args_time_in_force VARCHAR,
     args_type VARCHAR,
-    args_auto_borrow VARCHAR,
-    args_auto_repay VARCHAR,
+    args_auto_borrow BOOLEAN,
+    args_auto_repay BOOLEAN,
     args_client_oid VARCHAR,
     args_order_id VARCHAR,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
