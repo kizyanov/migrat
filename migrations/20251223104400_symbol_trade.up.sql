33a928b009618b2491fa3166c378037f2a0c1891
kizianov.ao <kizianov.ao@svrauto.ru>
2025-12-23 08:20:53 +0000

    .

--- migrations/20251223104400_symbol_trade.up.sql
+++ migrations/20251223104400_symbol_trade.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS symbol_trade (
     id SERIAL PRIMARY KEY,
     exchange VARCHAR NOT NULL,
     symbol VARCHAR NOT NULL,
+    size VARCHAR NOT NULL,
     enable BOOLEAN
 );
